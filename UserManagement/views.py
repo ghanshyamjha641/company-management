37c74acbf8955edf458f09ff6d3818873cd65eea
ghanshyamjha641 <ghanshyamjha641@gmail.com>
2025-06-01 23:05:26 +0530

    edit profile and logout added

--- UserManagement/views.py
+++ UserManagement/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import CustomUser
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.decorators import login_required
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages  # Optional, for flash messages
+
 
 
 # Create your views here.
@@ -40,8 +45,37 @@ def user_signup(request):
 
 
 def user_login(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        # Check if all fields are filled
+        if not email or not password:
+            return render(request, 'login.html', {'error': 'Email and password are required'})
+
+        # Authenticate user
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Replace 'home' with your desired URL name after login
+        else:
+            return render(request, 'login.html', {'error': 'Invalid email or password'})
+        
+
     return render(request,'login.html')
 
+#Home Page
+def home(request):
+    return render(request,'home.html')
+
+#logout
+def user_logout(request):
+    logout(request)
+    return redirect('login')
 
 
-
+#Edit Profile
+# @login_required
+def edit_profile(request):
+    # render edit form or process POST
+    return render(request, 'edit_profile.html')
